3a23e46605485fa5f5b347cebd9cf7a8b0dd05cf
Marcia Skousen <75454722+marcysko@users.noreply.github.com>
2021-06-26 10:20:50 -0700

    add shopping cart

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -20,10 +20,12 @@ const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useProductReducer({
-    products: [],
-    categories: [],
-    currentCategory: '',
-  });
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: ''
+});
   // use this to confirm it works!
   console.log(state);
   return <Provider value={[state, dispatch]} {...props} />;
